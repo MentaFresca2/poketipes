152aa109363e49ba39c4887a915e5fd7cdf4f380
Rubén <gutierrezruben657@gmail.com>
2024-01-02 21:13:47 +0100

    dios
    la de subir las cosas de un a e nuna no me la se, he corregido todo lo que estaba mal de figma y he subido las imagenes(svg).

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,62 @@
-import Image from 'next/image'
 
-export default function Home() {
+  import React from "react";
 
-
-  fetch('http://www.omdbapi.com/?apikey=c6e006b5&s=kung fu panda 4')
-  .then((response: Response) => response.json())
-  .then((data: any) => console.log(data));
-  return (
-    <div className='bg-gradient-to-b from-blue-500 to-blue-900 min-h-screen'>xd</div>
-  )
-}
+  export default function Home(){
+    return (
+      <div className="bg-[#1E1E1E] flex flex-row justify-center w-full">
+      <div className="bg-[#ce1212] w-[1661px] h-[912px]">
+        <div className="relative h-[912px]">
+          <div className="absolute w-[42px] h-[42px] top-[767px] left-[121px] bg-[#141414] rounded-[21px]" />
+          <div className="absolute w-[31px] h-[32px] top-[776px] left-[1594px] bg-[#bca900] rounded-full border-[3px] border-solid border-[#1e1e1e]" />
+          <div className="absolute w-[1661px] h-[912px] top-0 left-0">
+            <div className="absolute w-[384px] h-[106px] top-[33px] left-[40px]">
+              <div className="absolute w-[118px] h-[118px] top-[-6px] left-[-6px] bg-[#3c4597] rounded-full border-[6px] border-solid border-[#1e1e1e]" />
+              <div className="absolute w-[31px] h-[31px] top-[11px] left-[260px] bg-[#bca900] rounded-full border-[3px] border-solid border-[#1e1e1e]" />
+              <div className="absolute w-[31px] h-[31px] top-[11px] left-[356px] bg-[#0c9300] rounded-full border-[3px] border-solid border-[#1e1e1e]" />
+              <div className="absolute w-[31px] h-[31px] top-[11px] left-[164px] bg-[#830f0f] rounded-full border-[3px] border-solid border-[#1e1e1e]" />
+            </div>
+            <img className="absolute w-[804px] h-[76px] top-[100px] left-0" alt="Vector" src="vector-1.svg" />
+            <img className="absolute w-[782px] h-[76px] top-[100px] left-[879px]" alt="Vector" src="vector-4.svg" />
+            <img className="absolute w-[768px] h-[174px] top-0 left-[893px]" alt="Vector" src="vector-3.svg" />
+            <div className="absolute w-[100px] h-[912px] top-0 left-[793px] [background:linear-gradient(90deg,rgb(180,19,19)_0%,rgb(194.44,26.74,26.74)_51.04%,rgb(180,19,19)_100%)]">
+              <div className="absolute w-[100px] h-[27px] top-[161px] left-0 [background:linear-gradient(90deg,rgb(109.44,0,0)_0%,rgb(109.44,0,0)_0.01%,rgba(109.44,0,0,0.57)_49.48%,rgb(109.44,0,0)_100%)]" />
+              <div className="absolute w-[100px] h-[28px] top-[795px] left-0 [background:linear-gradient(90deg,rgb(109.44,0,0)_0%,rgba(109.44,0,0,0.57)_51.56%,rgb(109.44,0,0)_100%)]" />
+            </div>
+          </div>
+          <img className="absolute w-[68px] h-[16px] top-[751px] left-[177px]" alt="Rectangle" src="rectangle-7.svg" />
+          <img
+            className="absolute w-[68px] h-[16px] top-[735px] left-[1454px]"
+            alt="Rectangle"
+            src="rectangle-19.svg"
+          />
+          <img
+            className="absolute w-[69px] h-[16px] top-[735px] left-[1562px]"
+            alt="Rectangle"
+            src="rectangle-20.svg"
+          />
+          <img className="absolute w-[68px] h-[16px] top-[751px] left-[286px]" alt="Rectangle" src="rectangle-8.svg" />
+          <div className="absolute w-[650px] h-[469px] top-[242px] left-[87px]">
+            <div className="relative w-[651px] h-[470px] bg-[url(/vector-5.svg)] bg-[100%_100%]">
+              <div className="absolute w-[30px] h-[29px] top-[420px] left-[38px] bg-[#b21818] rounded-full border-2 border-solid border-[#1e1e1e]" />
+              <div className="absolute w-[27px] h-[26px] top-[42px] left-[370px] bg-[#b21919] rounded-full border-2 border-solid border-[#1e1e1e]" />
+              <div className="absolute w-[27px] h-[26px] top-[42px] left-[252px] bg-[#b21818] rounded-full border-2 border-solid border-[#1e1e1e]" />
+              <div className="absolute w-[25px] h-[23px] top-[422px] left-[584px]">
+                <div className="absolute w-[25px] h-[6px] top-0 left-0 bg-[#b21919] border-[0.7px] border-solid border-black" />
+                <div className="absolute w-[25px] h-[6px] top-[9px] left-0 bg-[#b21919] border-[0.7px] border-solid border-black" />
+                <div className="absolute w-[25px] h-[6px] top-[18px] left-0 bg-[#b21919] border-[0.7px] border-solid border-black" />
+              </div>
+              <div className="w-[568px] h-[319px] top-[80px] left-[40px] bg-[#282c34] absolute rounded-[20px]" />
+            </div>
+          </div>
+          <div className="w-[676px] h-[165px] top-[230px] left-[955px] bg-[#282c34] border border-solid border-black absolute rounded-[20px]" />
+          <div className="w-[258px] h-[91px] top-[804px] left-[225px] bg-[#516349] border border-solid border-black absolute rounded-[20px]" />
+          <img className="absolute w-[115px] h-[115px] top-[758px] left-[580px]" alt="Vector" src="vector-6.svg" />
+          <img className="absolute w-[149px] h-[75px] top-[742px] left-[955px]" alt="Group" src="group-5.png" />
+          <img className="absolute w-[676px] h-[270px] top-[441px] left-[955px]" alt="Group" src="group-4.png" />
+          <div className="w-[256px] h-[63px] top-[832px] left-[955px] bg-[#282c34] border border-solid border-black absolute rounded-[20px]" />
+          <div className="w-[256px] h-[63px] top-[832px] left-[1375px] bg-[#282c34] border border-solid border-black absolute rounded-[20px]" />
+        </div>
+      </div>
+    </div>
+    );
+  };

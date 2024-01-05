49e583d4008bc8a4be873a9d586487207c6d46e1
Rubén <gutierrezruben657@gmail.com>
2024-01-05 20:38:49 +0100

    xd
    he cambiado la img a un svg mejorado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,15 @@
     return (
       <div className="bg-[#1E1E1E] flex flex-row justify-center w-full">
         <Svgs svg="/poke/bug.svg" posini={100} posfin={100}/><Svgs svg="/poke/electric.svg" posini={100} posfin={100}/>
-      <div className="bg-[#C71212] w-[1661px] h-[912px] border-[#6D0000] border-2">
+      <div className="bg-[#ce1313] w-[1661px] h-[912px] border-[#6D0000] border-2">
         <div className="relative h-[912px]">
           <div className="absolute w-[42px] h-[42px] top-[767px] left-[121px] border-2 border-[#000000] bg-[#141414] rounded-[21px] shadow-plane" />
           <div className="absolute w-[31px] h-[32px] top-[776px] left-[1594px] bg-[#bca900] rounded-full border-[2px] border-solid border-[#000000] shadow-plane" />
           <div className="absolute w-[1661px] h-[912px] top-0 left-0">
             <div className="absolute w-[384px] h-[106px] top-[33px] left-[40px]">
               <div className=" bg-gradient-radial absolute w-[118px] h-[118px] top-[-6px] left-[-6px] rounded-full border-[6px] border-solid border-[#1e1e1e]" />
-              <div className="absolute w-[31px] h-[31px] top-[11px] left-[260px] bg-[#bca900] rounded-full border-[3px] border-solid border-[#1e1e1e]" />
-              <div className="absolute w-[31px] h-[31px] top-[11px] left-[356px] bg-[#0c9300] rounded-full border-[3px] border-solid border-[#1e1e1e]" />
+              <div className="absolute w-[31px] h-[31px] top-[11px] left-[260px] bg-gradient-radial-yel rounded-full border-[3px] border-solid border-[#1e1e1e]" />
+              <div className="absolute w-[31px] h-[31px] top-[11px] left-[356px] bg-gradient-radial-gre rounded-full border-[3px] border-solid border-[#1e1e1e]" />
               <div className="bg-gradient-radial-red absolute w-[31px] h-[31px] top-[11px] left-[164px] bg-[#830f0f] rounded-full border-[3px] border-solid border-[#1e1e1e]" />
             </div>
             <img className="absolute w-[804px] h-[76px] top-[100px] left-0" alt="Vector" src="vector-1.svg" />
@@ -39,9 +39,9 @@
           <img className="absolute w-[108px] h-[56px] top-[751px] left-[286px]" alt="Rectangle" src="rectangle-8.svg" />
           <div className="absolute w-[650px] h-[469px] top-[242px] left-[87px]">
             <div className="relative w-[716px] h-[517px] bg-[url(/vector-5.svg)] bg-[100%_100%] bg-no-repeat bg-contain ">
-              <div className="absolute w-[30px] h-[29px] top-[420px] left-[38px] bg-[#b21818] rounded-full border-2 border-solid border-[#1e1e1e]" />
-              <div className="absolute w-[27px] h-[26px] top-[42px] left-[370px] bg-[#b21919] rounded-full border-2 border-solid border-[#1e1e1e]" />
-              <div className="absolute w-[27px] h-[26px] top-[42px] left-[252px] bg-[#b21818] rounded-full border-2 border-solid border-[#1e1e1e]" />
+              <div className="absolute w-[30px] h-[29px] top-[420px] left-[38px] bg-[#b21818] rounded-full border-2 border-solid border-[#1e1e1e] shadow-lin" />
+              <div className="absolute w-[27px] h-[26px] top-[42px] left-[370px] bg-[#b21919] rounded-full border-2 border-solid border-[#1e1e1e] shadow-lin" />
+              <div className="absolute w-[27px] h-[26px] top-[42px] left-[252px] bg-[#b21818] rounded-full border-2 border-solid border-[#1e1e1e] shadow-lin" />
               <div className="absolute w-[25px] h-[23px] top-[422px] left-[584px]">
                 <div className="absolute w-[25px] h-[6px] top-0 left-0 bg-[#b21919] border-[0.7px] border-solid border-black" />
                 <div className="absolute w-[25px] h-[6px] top-[9px] left-0 bg-[#b21919] border-[0.7px] border-solid border-black" />
@@ -52,9 +52,9 @@
           </div>
           <div className="w-[676px] h-[165px] top-[230px] left-[955px] bg-[#282c34] border border-solid border-black absolute rounded-[20px] shadow-Lcustom-blue " />
           <div className="w-[258px] h-[91px] top-[804px] left-[225px] bg-[#516349] border border-solid border-black absolute rounded-[20px] shadow-LLcustom-blue" />
-          <img className="absolute w-[215px] h-[215px] top-[758px] left-[580px]" alt="Vector" src="vector-6.svg" />
+          <img className="absolute w-[215px] h-[215px] top-[758px] left-[580px]" alt="Vector" src="vector-6.svg"/>
           <img className="absolute w-[149px] h-[75px] top-[742px] left-[955px]" alt="Group" src="group-5.png" />
-          <img className="absolute w-[676px] h-[270px] top-[441px] left-[955px]" alt="Group" src="group-4.png" />
+          <img className="absolute w-[676px] h-[270px] top-[441px] left-[955px]" alt="Group" src="blu.svg" />
           <div className="w-[256px] h-[63px] top-[832px] left-[955px] bg-[#282c34] border border-solid border-black absolute rounded-[20px] shadow-LLcustom-blue" />
           <div className="w-[256px] h-[63px] top-[832px] left-[1375px] bg-[#282c34] border border-solid border-black absolute rounded-[20px] shadow-LLcustom-blue" />
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,12 +13,14 @@ const config: Config = {
         'Lcustom-blue': 'inset 0px -1px 24px rgba(0, 0, 0, 0.5)',
         'LLcustom-blue': 'inset 0px -1px 8px rgba(0, 0, 0, 0.8)',
         'r':'10px 0px 24px rgba(0, 0, 0, 0.1)',
-        'plane':'1.5px 1.5px 0px rgba(0, 0, 0, 1)'
+        'plane':'1.5px 1.5px 0px rgba(0, 0, 0, 1)',
+        'lin':'inset 1px 1px 4px rgba(0, 0, 0, 0.5)',
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(circle at center, #4C52D1 0%, #3c4597 50%, #3c4597 100%) ',
         'gradient-radial-red': 'radial-gradient(circle at center, #D12228 0%, #b21919 60%, #b21919 100%) ',
-        'gradient-radial-yel': 'radial-gradient(circle at center, #D12228 0%, #b21919 60%, #b21919 100%) ',
+        'gradient-radial-yel': 'radial-gradient(circle at center, #E2E21B 0%, #DAC400 50%, #DAC400 100%) ',
+        'gradient-radial-gre': 'radial-gradient(circle at center, #0FB100 0%, #0C9300 60%, #0C9300 100%) ',
           
       },
       borderWidth: {

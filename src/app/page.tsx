96fd287e3dc432e316af1dd38fa4b95bb73ef602
Rubén <gutierrezruben657@gmail.com>
2024-01-26 04:07:18 +0100

    Ahora en portatiles
    Pero no en moviles XD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -194,10 +194,10 @@ const noborrar = false;
 
 
     return (
-      <div className="flex flex-row justify-center overflow-hidden  w-[100%] h-[100%]">
-      <div className="bg-[#1E1E1E] flex justify-center w-[100%] h-[100%]">
+      
+      <div className="bg-[#1E1E1E] relative items-center w-[1661px] h-[912px]">
         
-      <div className="bg-[#ce1313] w-[89.5%] h-auto absolute border-[#6D0000] border-2">
+      <div className="bg-[#ce1313] w-auto h-auto relative border-[#6D0000] border-2">
         <div className="relative h-[912px]" >
           <div className="absolute w-[42px] h-[42px] top-[767px] left-[121px] border-2 border-[#000000] bg-[#141414] rounded-[21px] shadow-plane" />
           <div className="absolute w-[31px] h-[32px] top-[776px] left-[1594px] bg-[#bca900] rounded-full border-[2px] border-solid border-[#000000] shadow-plane" />
@@ -327,6 +327,6 @@ const noborrar = false;
         </div>
       </div>
     </div>
-  </div>
+  
     );
   };
